537529879c306f67377615b34f38a11145b318dd
Ana Khadivi <anakhadivi@icloud.com>
2014-03-13 13:06:52 -0400

    added object_project

---
+++ object_homework.rb
@@ -0,0 +1,14 @@
+class House
+	attr_accessor:color
+	attr_accessor:number_of_rooms
+	attr_accessor:number_of_bath
+	attr_accessor:floors
+	attr_accessor:basement
+	attr_accessor:attic
+	attr_accessor:yard
+end
+class Bathroom
+	attr_accessor:shower
+	attr_accessor:bath
+	attr_accessor:tiling
+end
