4cb7bd80c8f9a5705e1c276e0bc04dc76f579bb0
Ana Khadivi <anakhadivi@icloud.com>
2014-03-18 12:47:48 -0400

    fixed class inheritance

--- object_homework.rb
+++ object_homework.rb
@@ -1,14 +1,26 @@
 class House
+	def initialize(color, number_of_bedrooms)
+		self.color = color
+		self.number_of_bedrooms = number_of_bedrooms
+		
+	end
+	def fire_alarm
+		puts 'STOP DROP AND ROLL'	
+	end
+
 	attr_accessor:color
-	attr_accessor:number_of_rooms
-	attr_accessor:number_of_bath
-	attr_accessor:floors
-	attr_accessor:basement
-	attr_accessor:attic
-	attr_accessor:yard
+	attr_accessor:number_of_bedrooms
 end
-class Bathroom
+class Bathroom < House
+	def flush_toliet
+		puts 'FLUSHHHH!'		
+	end
+
 	attr_accessor:shower
 	attr_accessor:bath
 	attr_accessor:tiling
 end
+
+my_house = House.new('white', 3)
+puts my_house
+my_house.fire_alarm
